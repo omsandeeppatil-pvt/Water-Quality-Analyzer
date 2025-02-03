0ff65a09a6c7385a8377a0384b70326c32a85095
Om Patil <159125383+omsandeeppatil-pvt@users.noreply.github.com>
2025-02-03 12:04:41 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+/* ...Your other config rules */
+}
+
+module.exports = nextConfig
